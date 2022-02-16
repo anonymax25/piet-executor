ec66655e4b45ae9669c9726f51adffccc733bbbe
Maxime <maxime.dharboulle@gmail.com>
2022-02-16 03:35:21 +0100

    Add cors all origins $ methods

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: '*',
+    },
+  });
   app.use(morgan('tiny'));
   await app.listen(3000);
 }
